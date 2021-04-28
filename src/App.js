9614c590ba5a1fe979ea958728ef6bd8561fd5f7
roberterpi <robertpopct@gmail.com>
2021-04-28 20:59:29 +0300

    removed componen home from path

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Nav />
-      <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/shop" component={Shop} />
-      </Switch>
-    </div>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
     </Router>
   );
 }
@@ -25,6 +25,6 @@ const Home = () => {
   <div>
     <h1>Home</h1>
   </div>
-}
+};
 
 export default App;

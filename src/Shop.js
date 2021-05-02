2206f339e16fe3b9922032234e5c743f9ebbe054
roberterpi <robertpopct@gmail.com>
2021-05-02 16:21:32 +0300

    convert to json

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import './App.css';
 
 function Shop() {
+
+  useEffect(() => {
+    fetchItems();
+  },[]);
+
+  const fetchItems = async () => {
+    const data = await fetch('https://fakestoreapi.com/products');
+
+    const items = await data.json();
+    console.log(items);
+  }
+
+
   return (
     <div>
       <h1>Shop</h1>
